2ae86dbfb83c38761136e7f47d44e05374bdbb5c
wangmingyue <448335044@qq.com>
2020-07-16 10:36:47 +0800

    router

--- src/componments/content/content.jsx
+++ src/componments/content/content.jsx
@@ -1,5 +1,11 @@
 import React, {Component} from 'react'
+import {Redirect, Route, Switch} from 'react-router-dom'
 import './content.less'
+
+// 引入自定义组件
+// import Admin from '../../pages/admin/admin'
+import One from '../../pages/one/one'
+import Two from '../../pages/two/two'
 
 
 export default class Content extends Component {
@@ -9,8 +15,21 @@ export default class Content extends Component {
     render() {
 
       return (
-        <div className='header'>
-          neirong 
+        <div className='content-box'>
+          <Switch>
+            <Redirect from='/' exact to='/admin'/>
+            <Route path='/one' component={One}/>
+            <Route path='/two' component={Two}/>
+            {/* <Route path='/category' component={Category}/>
+            <Route path='/product' component={Product}/>
+            <Route path='/user' component={User}/>
+            <Route path='/role' component={Role}/>
+            <Route path="/charts/bar" component={Bar}/>
+            <Route path="/charts/pie" component={Pie}/>
+            <Route path="/charts/line" component={Line}/>
+            <Route path="/order" component={Order}/> */}
+            {/* <Route component={NotFound}/> */}
+          </Switch>
         </div>
       )
     }
---
+++ src/pages/one/one.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+export default class One extends Component {
+    render () {
+        return (
+            <div>one组件</div>
+        )
+    }
+}
